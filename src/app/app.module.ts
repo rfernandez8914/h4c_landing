d368973d43556edb86b181374281dcb92065c910
Ricardo Fernandez <proyectosolunatec@gmail.com>
2023-10-01 22:13:10 -0600

    WH4C-1080 - Homepage loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -25,7 +24,6 @@ import { TopUserComponent } from './components/top-user/top-user.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule,
     NgxRangeModule,
     ReactiveFormsModule,
     FormsModule,
